b7477c6131a00166bcff71f2d7db55fe303b2503
JNAray <99225767+JNAray@users.noreply.github.com>
2022-02-08 20:41:29 -0300

    Add files via upload

---
+++ ejer4.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author nicoa
+ */
+public class ejer4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        Rectangulo n1 = new Rectangulo ();
+        n1.rellenar();
+        n1.perimetro();
+        n1.superficie();
+    }
+    
+}
